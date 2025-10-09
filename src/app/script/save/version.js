473121dd5bb297d42e2569ada340db3d21718425
Ryu <112185174+pw56@users.noreply.github.com>
2025-10-09 17:09:45 +0900

    クライアント側のアプリのバージョン管理のライブラリを開発

---
+++ src/app/script/save/version.js
@@ -0,0 +1,27 @@
+function versionManager() {
+  const STORAGE_KEY = "lastVisitedVersion";
+  const PREVIOUS_VERSION = localStorage.getItem(STORAGE_KEY);
+
+  return {
+    getVersion() {
+      return localStorage.getItem(STORAGE_KEY);
+    },
+
+    /**
+     * 現在のバージョンを設定
+     * @param {string} currentVersion - 現在のアプリバージョン（例: "2.0.0"）
+     */
+    setVersion(currentVersion) {
+      // ガード節
+      if(currentVersion === getVersion()) return;
+
+      // ローカルストレージに入力されたバージョンを保存
+      localStorage.setItem(STORAGE_KEY, currentVersion);
+    },
+
+    // 現在のバージョンが前回からアップデートされているかを取得
+    isUpdated() {
+      return (getVersion() !== PREVIOUS_VERSION);
+    }
+  };
+};
