e56df2ecd4dbdd118817b7ded1583394ef04ce2d
Ryu <112185174+pw56@users.noreply.github.com>
2025-11-23 13:14:18 +0900

    バンドルの影響を考慮してパスを修正

--- src/functions/sw.js
+++ src/functions/sw.js
@@ -1,5 +1,12 @@
 const CACHE_NAME = 'web-puri-cache';
-const urlsToCache = ['../../app', '../../assets']; // キャッシュ対象に機械学習モデルを追加予定(外部リンクのCORSエラーとか、わからん)
+const urlsToCache = [
+  '../build', // 本番環境にリリースするときは、'../build'から'../app'に変更
+  '../assets',
+  'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.22.0/dist/tf.min.js',
+  'https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation@0.1/selfie_segmentation.js',
+  'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/face_mesh.js',
+  'https://cdn.jsdelivr.net/npm/@tensorflow-models/face-landmarks-detection/dist/face-landmarks-detection.js'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
