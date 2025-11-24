1dd22f92bbe055420427932432cc9005d7d9dab1
Ryu <112185174+pw56@users.noreply.github.com>
2025-11-24 18:52:17 +0900

    Viteの設定ファイルを作成

---
+++ src/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Vite 設定
+export default defineConfig({
+  plugins: [react()],
+  root: './src', // プロジェクトのエントリを src に設定
+  build: {
+    outDir: '../build', // ビルド成果物を build に出力
+    emptyOutDir: true, // ビルド前に出力先を空にする
+    minify: true, // ミニフィケーションを有効化
+  },
+  resolve: {
+    alias: {
+      '@components': './components',
+      '@pages': './pages',
+      '@functions': './functions',
+    },
+  },
+});
