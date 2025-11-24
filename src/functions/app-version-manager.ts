3ba0122bfce80d54f32738f77694893cf18dff85
Ryu <112185174+pw56@users.noreply.github.com>
2025-11-24 12:44:25 +0900

    ソフトプライベートをハードプライベートに移行

--- src/functions/app-version-manager.ts
+++ src/functions/app-version-manager.ts
@@ -1,13 +1,13 @@
 export class AppVersionManager {
-  private readonly STORAGE_KEY:string = "lastVisitedVersion";
-  private readonly previousVersion: string | null = localStorage.getItem(this.STORAGE_KEY);
+  readonly #STORAGE_KEY:string = "lastVisitedVersion";
+  readonly #previousVersion: string | null = localStorage.getItem(this.#STORAGE_KEY);
 
   /**
    * 文字列型かチェック
    * @throw 文字列型の例外
    */
   constructor(initialVersion: string) {
-    this.setVersion(initialVersion);
+    this.#setVersion(initialVersion);
   }
 
   /**
@@ -15,18 +15,18 @@ export class AppVersionManager {
    * @returns {string | null} 現在のアプリのバージョン（例: "1.0.0"）
    */
   getVersion(): string | null {
-    const result:string | null = localStorage.getItem(this.STORAGE_KEY);
+    const result:string | null = localStorage.getItem(this.#STORAGE_KEY);
     return result;
   }
 
   /**
    * バージョンを保存する（プライベート）
    */
-  private setVersion(version: string): void {
+  #setVersion(version: string): void {
     if (typeof version !== "string") {
       throw new Error("入力値が文字列ではありません。");
     }
-    localStorage.setItem(this.STORAGE_KEY, version);
+    localStorage.setItem(this.#STORAGE_KEY, version);
   }
 
   /**
@@ -34,7 +34,7 @@ export class AppVersionManager {
    * @returns {boolean} 更新されたか否か
    */
   isUpdated(): boolean {
-    const result: boolean = (this.getVersion() !== this.previousVersion);
+    const result: boolean = (this.getVersion() !== this.#previousVersion);
     return result;
   }
 };
