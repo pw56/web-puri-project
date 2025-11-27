77fcc8db67035e60ce0924a5066e21ac9c11f16a
Ryu <112185174+pw56@users.noreply.github.com>
2025-11-27 20:19:29 +0900

    TSの出力先の切り替え設定を追加

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Vite 設定
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  root: './src', // プロジェクトのエントリを src に設定
+  root: './src',
   build: {
-    outDir: '../dist', // ビルド成果物を dist に出力
-    emptyOutDir: true, // ビルド前に出力先を空にする
-    minify: true, // ミニフィケーションを有効化
+    outDir: mode === 'production' ? '../dist_release' : '../dist_debug',
+    emptyOutDir: true,
+    minify: mode === 'production',
   },
   resolve: {
     alias: {
       '@components': './components',
-      '@pages': './pages',
-      '@functions': './functions',
+      '@features': './features',
     },
   },
-});
+}));
