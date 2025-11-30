798d4fe4f93b1815d80dd949a614cdeb6f61cfcd
Ryu <112185174+pw56@users.noreply.github.com>
2025-11-30 12:54:16 +0900

    デバッグ用の関数(consoleをオーバーライド)を用意

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     alias: {
       '@components': './components',
       '@features': './features',
+      '@debug': '../tests'
     },
   },
 }));
---
+++ tests/debug.js
@@ -0,0 +1,3 @@
+console.log = (text) => { alert(`Log: ${text}`); };
+console.warn = (text) => { alert(`Warn: ${text}`); };
+console.error = (text) => { alert(`Error: ${text}`); };
