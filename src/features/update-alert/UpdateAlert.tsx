1c64978dca231b002a46c12ca8066fd7bb36ea75
Ryu <112185174+pw56@users.noreply.github.com>
2025-11-30 13:19:18 +0900

    アプリのポップアップをappオブジェクトに集約

--- src/features/update-alert/UpdateAlert.tsx
+++ src/features/update-alert/UpdateAlert.tsx
@@ -5,6 +5,6 @@ import { AppVersionManager } from './utils/app-version-manager';
 window.addEventListener('load', () => {
   const appVersion = new AppVersionManager('ここは ./release/version.yml から読み込む');
   if(appVersion.isUpdated()) {
-    // notice('ここは ./release/message.md から読み込む');
+    // app.alert('ここは ./release/message.md から読み込む');
   }
 });
