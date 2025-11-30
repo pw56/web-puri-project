fdaa459bd6778ca2c675c7fa5ca259f709dc6bfe
Ryu <112185174+pw56@users.noreply.github.com>
2025-11-30 16:10:39 +0900

    アイコンのパスを絶対パスに変更

--- tests/debug/index.js
+++ tests/debug/index.js
@@ -6,7 +6,7 @@ Notification.requestPermission().then(() => {
 
 function sendNotification(type, text) {
   let displayType = '',
-      iconPath = '/web-puri-project/tests/debug/assets/';
+      iconPath = 'https://pw56.github.io/web-puri-project/tests/debug/assets/';
   switch (type) {
     case 'log': {
       displayType = 'ログ';
