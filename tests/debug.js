16a6ca38d5d87192ad3e035481aab6f7fc24ed7e
Ryu <112185174+pw56@users.noreply.github.com>
2025-11-30 13:07:00 +0900

    デバッグ用の関数を更新

--- tests/debug.js
+++ tests/debug.js
@@ -1,3 +1,29 @@
-console.log = (text) => { alert(`Log: ${text}`); };
-console.warn = (text) => { alert(`Warn: ${text}`); };
-console.error = (text) => { alert(`Error: ${text}`); };
+let isNotificationGranted = false;
+Notification.requestPermission().then(() => {
+  isNotificationGranted = true;
+});
+
+function sendNotification(type, text) {
+  let displayType = '';
+  switch (type) {
+    case 'log': {
+      displayType = 'ログ';
+      break;
+    }
+
+    case 'warn': {
+      displayType = '警告';
+      break;
+    }
+
+    case 'error': {
+      displayType = 'エラー';
+      break;
+    }
+  }
+  alert(`${displayType}: ${text}`);
+}
+
+console.log = (text) => { sendNotification('log', text); };
+console.warn = (text) => { sendNotification('warn', text); };
+console.error = (text) => { sendNotification('error', text) };
