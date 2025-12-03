0bfc82f288b4af94382c33f71901744d1e45a27a
Ryu <112185174+pw56@users.noreply.github.com>
2025-12-03 19:59:45 +0900

    ライブラリのエイリアスを廃止

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig(({ mode }) => ({
     alias: {
       '@components': './components',
       '@features': './features',
-      '@libs': './libs',
       '@debug': '../tests'
     },
   },
