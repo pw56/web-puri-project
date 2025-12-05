ffacaf6d1c288dd4319d7c139f1b98fbb6294564
Ryu <112185174+pw56@users.noreply.github.com>
2025-12-05 19:01:28 +0900

    Appコンポーネント専用のフォルダを作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import '@debug';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './app/App';
 
 // レンダリング
 const root = ReactDOM.createRoot(document.getElementById('root'));
