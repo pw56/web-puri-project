7d6020ee5e1a26c3aaa7243da81e4126fc96ac62
Ryu <112185174+pw56@users.noreply.github.com>
2025-12-06 18:05:05 +0900

    離脱警告

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,26 @@
+import { useEffect } from 'react';
 import styles from './App.module.css';
 import '@features/pages';
 import '@features/update-alert/UpdateAlert';
 
 const App = () => {
+  useEffect(() => {
+    const handleBeforeUnload = (e) => {
+      e.preventDefault();
+    };
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
   return (
     <div className={styles.App}>
       {/* <StartPage /> */}
       {/* <ResultPage /> */}
     </div>
   );
-}
-
-// 誤って編集内容が失われるのを防ぐため、ページの離脱を警告
-window.addEventListener('beforeunload',(e)=>{
-  e.preventDefault();
-});
+};
 
 export default App;
