b06ddb6406c6bd5144e8543c25b6cb5f300cfb98
Ryu <112185174+pw56@users.noreply.github.com>
2025-12-06 16:39:20 +0900

    manifest.jsonの生成を自動化

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,8 +1,43 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      srcDir: './',
+      filename: './app/sw.js',
+      strategies: 'injectManifest',
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      manifest: {
+        name: 'Webプリ',
+        short_name: 'Webプリ',
+        author: 'pw56',
+        description: 'このアプリは、AIや画像処理などの最新技術を使ったWebアプリケーションです。',
+        start_url: './index.html',
+        scope: './',
+        display: 'standalone',
+        background_color: '#ff87c5',
+        theme_color: '#ff87c5',
+        icons: [
+          {
+            src: '/assets/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/assets/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   root: './',
   base: '/web-puri-project/',
   build: {
