de76a585c02cb553109360de76859b1c3d251641
Ryu <112185174+pw56@users.noreply.github.com>
2025-12-07 21:31:15 +0900

    アップデート通知カスタムフックに変更

---
+++ src/features/update-alert/use-update-alert.ts
@@ -0,0 +1,43 @@
+import '@debug';
+import '@components/Alert';
+import { AppVersionManager } from './utils/app-version-manager';
+import { fetchTextFile } from "./utils/fetch-text-file";
+import 'yaml';
+
+let appVersion: AppVersionManager;
+
+async function getVersion(): Promise<string> {
+  const path: string = "/assets/release/version.yml";
+  const versionText: string = await fetchTextFile(path)
+  .catch((error) => {
+    throw new Error(`アップデートバージョンファイルの読み取りでエラーが発生しました ${error}`);
+  });
+
+  const versionObject: Object = YAML.parse(versionText);
+  const version: string = versionObject.app_version || '1.0.0';
+  return version;
+}
+
+async function openVersionManager(): Promise<void> {
+  const VERSION_ID: string = "window-app-version";
+  appVersion = new AppVersionManager(VERSION_ID, await getVersion());
+
+  console.log(`現在のアプリのバージョン: ${await appVersion.getVersion()}`);
+  console.log(`アップデートされたか: ${await appVersion.isUpdated()}`);
+}
+
+export async function useUpdateAlert(): Promise<void> {
+  const alert = useAlert();
+  await openVersionManager();
+
+  // アップデートがあったら通知
+  useEffect(() => {
+    const checkUpdate = async () => {
+      if (await appVersion.isUpdated()) {
+        alert({contentUrl: '/assets/release/message.md'});
+      }
+    };
+
+    checkUpdate();
+  }, [alert]);
+}
