607a45b3efe45fe6b739abfc9cfabc8d54189740
Ryu <112185174+pw56@users.noreply.github.com>
2025-12-20 16:57:30 +0900

    プリ画像管理クラスにファイル取得メソッドを追加

--- src/utils/puri-images-class/puri-images-class.ts
+++ src/utils/puri-images-class/puri-images-class.ts
@@ -21,13 +21,13 @@ export class PuriImages {
   }
 
   addImageFromBlob(image: Blob): void {
-    const puri:Puri = new Puri(image);
+    const puri: Puri = new Puri(image);
     this.#puris.push(puri);
   }
 
   addImagesFromBlob(images: Blob[]): void {
     images.forEach((image) => {
-      const puri:Puri = new Puri(image);
+      const puri: Puri = new Puri(image);
       this.#puris.push(puri);
     });
 
@@ -41,10 +41,39 @@ export class PuriImages {
     return await this.#puris[index].getImageAsBlob();
   }
 
+  async getImageAsFile(index: number): Promise<Blob> {
+    const blob = await this.#puris[index].getImageAsBlob();
+    
+    /*
+       ファイル名は日時(yyyy-mm-dd-hh-mm形式)
+        例: "2025-12-20-16-36"
+      */
+    const dateTime: string =
+      new Intl.DateTimeFormat('sv-SE', {
+        dateStyle: 'short',
+        timeStyle: 'short'
+      }).format(this.#date ?? undefined).replace(' ', '-').replace(':', '-');
+
+    /*
+      出力されるファイル名は
+      "puri" + 日時 + 枚数のインデックス
+      となり、文字列はハイフンで結合される
+      
+      例:
+      "puri-2025-06-15-09-32-1"
+    */
+    const fileName: string = `puri-${dateTime}-${index}`;
+
+    return new File(
+      [blob],
+      fileName
+    );
+  }
+
   getImageAsUrl(index: number): string {
     return this.#puris[index].getImageAsUrl();
   }
-  
+
   getAllImagesAsImage(): HTMLImageElement[] {
     return this.#puris.map((puri) => {
       return puri.getImageAsImage();
@@ -55,6 +84,38 @@ export class PuriImages {
     return await Promise.all(
       this.#puris.map((puri) => puri.getImageAsBlob())
     );
+  }
+
+  async getAllImagesAsFile(): Promise<File[]> {
+    const blobImages: Blob[] = await Promise.all(
+      this.#puris.map((puri) => puri.getImageAsBlob())
+    );
+    return blobImages.map((blob, index) => {
+      /*
+       ファイル名は日時(yyyy-mm-dd-hh-mm形式)
+        例: "2025-12-20-16-36"
+      */
+      const dateTime: string =
+        new Intl.DateTimeFormat('sv-SE', {
+          dateStyle: 'short',
+          timeStyle: 'short'
+        }).format(this.#date ?? undefined).replace(' ', '-').replace(':', '-');
+
+      /*
+        出力されるファイル名は
+        "puri" + 日時 + 枚数のインデックス
+        となり、文字列はハイフンで結合される
+        
+        例:
+        "puri-2025-06-15-09-32-1"
+      */
+      const fileName: string = `puri-${dateTime}-${index}`;
+
+      return new File(
+        [blob],
+        fileName
+      );
+    });
   }
 
   getAllImagesAsUrl(): string[] {
@@ -75,7 +136,7 @@ export class PuriImages {
     this.#location = location;
   }
 
-  getLocation(): GeoLocation | null{
+  getLocation(): GeoLocation | null {
     return this.#location;
   }
 }
