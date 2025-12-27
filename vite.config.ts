ef2e2c1e821a3fe8a233eb8bf6e4378715ec103c
Ryu <112185174+pw56@users.noreply.github.com>
2025-12-27 13:04:44 +0900

    設定ファイルをルートに移動

--- vite.config.ts
+++ vite.config.ts
@@ -2,44 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
+const base = '/web-puri-project/';
+
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    VitePWA({
-      srcDir: './',
-      filename: './app/sw.js',
-      strategies: 'injectManifest',
-      registerType: 'autoUpdate',
-      devOptions: { enabled: true },
-      manifest: {
-        name: 'Webプリ',
-        short_name: 'Webプリ',
-        author: 'pw56',
-        description: 'このアプリは、AIや画像処理などの最新技術を使ったWebアプリケーションです。',
-        start_url: './index.html',
-        scope: './',
-        display: 'standalone',
-        background_color: '#ff87c5',
-        theme_color: '#ff87c5',
-        icons: [
-          {
-            src: '/assets/icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any maskable'
-          },
-          {
-            src: '/assets/icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
-  ],
   root: './',
-  base: '/web-puri-project/',
+  base: base,
   build: {
     outDir: mode === 'production' ? '../app' : '../dist',
     emptyOutDir: true,
@@ -53,4 +20,39 @@ export default defineConfig(({ mode }) => ({
       '@debug': '../tests'
     },
   },
+  plugins: [
+    react(),
+    VitePWA({
+      srcDir: './src/',
+      filename: './src/app/sw.js',
+      strategies: 'injectManifest',
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      manifest: {
+        name: 'Webプリ',
+        short_name: 'Webプリ',
+        author: 'pw56',
+        description: 'このアプリは、AIや画像処理などの最新技術を使ったWebアプリケーションです。',
+        start_url: './',
+        scope: './',
+        display: 'standalone',
+        background_color: '#ff87c5',
+        theme_color: '#ff87c5',
+        icons: [
+          {
+            src: `${base}public/icons/icon-192x192.png`,
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: `${base}public/icons/icon-512x512.png`,
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 }));
