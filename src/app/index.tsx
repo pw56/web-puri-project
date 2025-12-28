8f7460f64ca46d9258bb6f143b92974c772a0e98
Ryu <112185174+pw56@users.noreply.github.com>
2025-12-28 13:10:01 +0900

    もしかしたらswのレジスタ登録が原因

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,13 @@
 import '@debug';
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
+import { registerSW } from 'virtual:pwa-register';
+
+// プラグインでPWAのService Workerを登録
+registerSW({
+  immediate: true,
+});
 
 // レンダリング
 const root = createRoot(document.getElementById('root')!);
 root.render(<App />);
-
-// オフライン対応
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./app/sw.ts')
-      .then(reg => console.log('Service Workerの登録に成功', reg))
-      .catch(err => console.error('Service Workerの登録に失敗:', err));
-  });
-}
