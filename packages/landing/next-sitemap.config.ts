1d26378a6213c59e681e3d56aa65d99ec629555f
Ryu <112185174+pw56@users.noreply.github.com>
2026-01-01 21:22:17 +0900

    Next.jsのサイトマップ生成プラグインの設定ファイルを作成

---
+++ packages/landing/next-sitemap.config.ts
@@ -0,0 +1,11 @@
+import type { IConfig } from 'next-sitemap';
+
+const config: IConfig = {
+  siteUrl: 'https://pw56.github.io/web-puri-project',
+  generateRobotsTxt: false,
+  outDir: './out',
+  changefreq: 'daily',
+  priority: 1,
+}
+
+export default config;
