bf898722166ae0decf37df192a5f394c066837ce
Ryu <112185174+pw56@users.noreply.github.com>
2026-01-02 21:58:43 +0900

    Viteでフォントの読み込みを移行

--- apps/pwa/index.tsx
+++ apps/pwa/index.tsx
@@ -1,5 +1,6 @@
 import '@debug';
 import { createRoot } from 'react-dom/client';
+import '@fontsource/hachi-maru-pop';
 import App from './app/App';
 import { registerSW } from 'virtual:pwa-register';
 
