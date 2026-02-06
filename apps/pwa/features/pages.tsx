e6069582b0179cfb4b039e5cc0e6cd8f3233ba0b
Ryu <112185174+pw56@users.noreply.github.com>
2026-02-06 20:57:05 +0900

    Reactファイルのフォルダ構造整理

--- apps/pwa/features/pages.tsx
+++
@@ -1,5 +0,0 @@
-// import StartPage from './start/StartPage';
-// import ShootPage from './shoot/ShootPage';
-// import EditPage from './edit/EditPage';
-// import ResultPage from './result/ResultPage';
-// import PickedPage from './picked/PickedPage';
---
+++ apps/pwa/pages/pages.tsx
@@ -0,0 +1,5 @@
+// import StartPage from './start-page/StartPage';
+// import ShootPage from './shoot-page/ShootPage';
+// import EditPage from './edit-page/EditPage';
+// import ResultPage from './result-page/ResultPage';
+// import PickedPage from './picked-page/PickedPage';
